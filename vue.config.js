7a196b0b1e754332be963f349a1758a45cb6fd30
aliceaisi <als0221@hotmail.com>
2020-03-31 16:08:21 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ var ZipPlugin = require('zip-webpack-plugin')
 module.exports = {
   publicPath: './',
   outputDir: 'dist',
+  assetsDir: 'static',
   configureWebpack: {
     plugins: [new ZipPlugin({
       pathPrefix: '', // 打包到对应目录（一般为当前目录'./'）
